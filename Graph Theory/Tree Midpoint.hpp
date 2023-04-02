026cd79ff2fc658321f6ac2103532f50661a1273
MuHe <oliver.he@tum.de>
2023-04-02 20:10:42 +0200

    update the midpoints

--- Graph Theory/Tree Midpoint.hpp
+++ Graph Theory/Tree Midpoint.hpp
@@ -3,21 +3,23 @@
 
 struct GetD {
 public:
-	int n, rt, len;
+	int n, rt, len, rt2;
 
 private:
 	std::vector<std::vector<int>> e;
 	std::vector<int> md, sed, mdf, sedf, upd;
 
+public: 
 	GetD(int __n, std::vector<std::vector<int>> __e) : n(__n), e(__e) {
 		md.resize(n + 1);
 		sed.resize(n + 1);
 		mdf.resize(n + 1);
 		sedf.resize(n + 1);
 		upd.resize(n + 1);
-        len = 0;
+		len = 0, rt = rt2 = -1;
 	}
 
+private:
 	void dfs(int u, int fa) {
 		md[u] = sed[u] = 0;
 		for (int v : e[u]) {
@@ -58,5 +60,9 @@ public:
 		for (int i = 1; i <= n; ++i)
 			if (std::max(upd[i], md[i]) < maxd)
 				maxd = std::max(upd[i], md[i]), rt = i;
+			else if (std::max(upd[i], md[i]) == maxd)
+				rt2 = i;
+		if (std::max(upd[rt], md[rt]) != std::max(upd[rt2], md[rt2]))
+			rt2 = -1;
 	}
 };
